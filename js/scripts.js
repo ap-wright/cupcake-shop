b913f84e8bd63127524d9bc9a3c6d8328af5874e
April Wright <jham13@yahoo.com>
2017-09-01 14:22:04 +1000

    Add some jquery effects like slideDown, fadeOut and slideToggle.

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,31 @@ $(document).ready(function() {
     $(".2image-hidden").faceOut();
   });
 });
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+    });
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut("slow");
+        $("#div3").fadeOut(3000);
+    });
+});
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#recipe").slideDown("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
